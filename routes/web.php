8211853997b88df5d1791acd1c447dbca7b08da5
bintang4974 <bintang.bintangramadhan4974@gmail.com>
2020-09-28 11:48:56 +0700

    menambahkan route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Closure
+Route::get('/profil', function () {
+    return "ini adalah profil siswa";
+})->name('profil');
+
+// Route name
+Route::get('/tesname', function () {
+    return route('profil');
+});
+
+// Parameter
+Route::get('/profil/{name}', function ($name) {
+    return $name;
+});
+
+// Controller
+Route::get("/testcontroller", "TestController@show");
+
+// Resource
+Route::resource('siswa', 'SiswaController');
