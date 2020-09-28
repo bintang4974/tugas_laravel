7fa931897289c936c13bf7d415866f1ca9184b87
bintang4974 <bintang.bintangramadhan4974@gmail.com>
2020-09-28 16:16:44 +0700

    menambahkan routes daring 2

--- routes/web.php
+++ routes/web.php
@@ -2,38 +2,36 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-// Closure
-Route::get('/profil', function () {
-    return "ini adalah profil siswa";
-})->name('profil');
-
-// Route name
-Route::get('/tesname', function () {
-    return route('profil');
+Route::get('/about', function () {
+    return view('about');
 });
 
-// Parameter
-Route::get('/profil/{name}', function ($name) {
-    return $name;
-});
 
-// Controller
-Route::get("/testcontroller", "TestController@show");
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-// Resource
-Route::resource('siswa', 'SiswaController');
+// // Closure
+// Route::get('/profil', function () {
+//     return "ini adalah profil siswa";
+// })->name('profil');
+
+// // Route name
+// Route::get('/tesname', function () {
+//     return route('profil');
+// });
+
+// // Parameter
+// Route::get('/profil/{name}', function ($name) {
+//     return $name;
+// });
+
+// // Controller
+// Route::get("/testcontroller", "TestController@show");
+
+// // Resource
+// Route::resource('siswa', 'SiswaController');
